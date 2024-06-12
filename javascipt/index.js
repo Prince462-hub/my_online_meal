6b932a70bbbe973ef4e8490e56656dc8441ea80e
Prince  Kumar <krprince462@gmail.com>
2024-06-12 18:08:55 +0530

    initial commit

---
+++ javascipt/index.js
@@ -0,0 +1,9 @@
+let parent = document.querySelector('#navbar ul').parentElement;
+
+// while (parent) {
+//     const hasOverflow = getComputedStyle(parent).overflow;
+//     if (hasOverflow !== 'visible') {
+//         console.log(hasOverflow, parent);
+//     }
+//     parent = parent.parentElement;
+// }
